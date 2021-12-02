52a8645dfc2e8c4a6fdef7eebaa1482b4a83ae61
praveenkumar <praveen_kumar2@condenast.com>
2021-12-02 19:21:19 +0530

    process

--- bin/time_validation.sh
+++ bin/time_validation.sh
@@ -2,7 +2,7 @@
 
 current_time=$(date +%H:%M)
 
-if [[ "$current_time" > "16:00" ]] && [[ "$current_time" < "18:30" ]]; then
+if [[ "$current_time" > "19:00" ]] && [[ "$current_time" < "20:00" ]]; then
      echo "Im in"
    else
      echo "Exiting"
